9e36f0696ef9f5782bda5b24b8dac80955f666f2
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-12-14 21:20:41 -0800

    test corrected snaps updated

--- src/views/Home.test.jsx
+++ src/views/Home.test.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Home from './Home';
+import { UserContext } from '../context/UserContext';
 
 it('should render the Home view', () => {
-  const { container } = render(<Home />);
+  const { container } = (
+    <UserContext>
+      <Home />
+    </UserContext>
+  );
   expect(container).toMatchSnapshot();
 });
